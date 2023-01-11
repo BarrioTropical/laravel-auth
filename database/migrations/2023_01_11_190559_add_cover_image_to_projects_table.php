1a581b788faf4523bdc3984086a6826111e61beb
“BarrioTropical” <barrio>
2023-01-11 20:10:15 +0100

    migration project cover image db
    Signed-off-by: “BarrioTropical” <“barrio>

--- database/migrations/2023_01_11_190559_add_cover_image_to_projects_table.php
+++ database/migrations/2023_01_11_190559_add_cover_image_to_projects_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('posts', function (Blueprint $table) {
+        Schema::table('projects', function (Blueprint $table) {
             $table->string('cover_image')->nullable()->after('id');
         });
     }
@@ -25,7 +25,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('posts', function (Blueprint $table) {
+        Schema::table('projects', function (Blueprint $table) {
             $table->dropColumn('cover_image');
         });
     }
